f6f8e497e40355f5f495901d4c7e40c71aef58b4
claudia254 <akiruclaudia@gmail.com>
2024-11-08 18:24:59 +0300

    initial commit

---
+++ javaapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from javaapp import views
+
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('contact',views.contact,name='contact'),
+    path('trainer',views.trainer,name='trainer'),
+    path('why',views.why,name='why'),
+
+]
